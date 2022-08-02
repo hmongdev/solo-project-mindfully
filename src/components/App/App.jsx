fb65219f24edb1b8de848e54daaff0c07a9a8e3a
Avery Yang <averyyang2674@gmail.com>
2022-08-01 22:43:19 -0500

    remove aboutPage.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,7 +12,6 @@ import Nav from '../Nav/Nav';
 
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
-import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
@@ -37,16 +36,6 @@ function App() {
                 <Switch>
                     {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
                     <Redirect exact from="/" to="/login" />
-
-                    {/* Visiting localhost:3000/about will show the about page. */}
-                    <Route
-                        // shows AboutPage at all times (logged in or not)
-                        exact
-                        path="/about"
-                    >
-                        <AboutPage />
-                    </Route>
-
                     {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
