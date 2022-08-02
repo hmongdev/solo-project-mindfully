1005cf18bb7011d0616887658f7e11cb41ad383d
Avery Yang <averyyang2674@gmail.com>
2022-08-02 14:30:25 -0500

    create profile component and moved logout button there

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import './Dashboard.css';
 
 function Dashboard() {
-    // this component doesn't do much to start, just renders some user reducer info to the DOM
-    const user = useSelector((store) => store.user);
     const dispatch = useDispatch();
     return (
         <div className="container">
             <h1 className="view-title center">Dashboard</h1>
             {/* Put this logout button on profile screen */}
             {/* <LogOutButton className="btn center" /> */}
-            <div className="center">
-                <button
-                    className="btn"
-                    onClick={() => dispatch({ type: 'LOGOUT' })}
-                >
-                    Log Out
-                </button>
-            </div>
+            <div className="quote-container center">Quotes</div>
+            <div className="center"></div>
         </div>
     );
 }
