a2656969ad560b255a3e3c0ee10eac845d5a662c
Avery Yang <averyyang2674@gmail.com>
2022-08-02 15:59:09 -0500

    add mui and scaffolding for dashboard

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import * as React from 'react';
 import { useHistory } from 'react-router-dom';
+import '../Navbar/Navbar.css';
+
+//mui
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export default function Navbar() {
     const history = useHistory();
@@ -7,11 +13,28 @@ export default function Navbar() {
     return (
         <div className="navbar">
             <button
+                className="practice"
                 onClick={() => {
-                    history.push('/login');
+                    history.push('/practices');
                 }}
             >
-                Practice
+                <PsychologyIcon style={{ fontSize: 60 }} />
+            </button>
+            <button
+                className="profile"
+                onClick={() => {
+                    history.push('/profile');
+                }}
+            >
+                <AccountCircleIcon style={{ fontSize: 60 }} />
+            </button>
+            <button
+                className="checkins"
+                onClick={() => {
+                    history.push('/checkins');
+                }}
+            >
+                <FormatListBulletedIcon style={{ fontSize: 60 }} />
             </button>
         </div>
     );
