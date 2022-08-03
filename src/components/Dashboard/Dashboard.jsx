c65020d6df07c7d22b445240d1313f8ef8449cd0
Avery Yang <averyyang2674@gmail.com>
2022-08-02 21:21:03 -0500

    add checkin-history to dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -5,16 +5,11 @@ import '../Navbar/Navbar.css';
 import Navbar from '../Navbar/Navbar';
 import CheckinsList from '../CheckinsList/CheckinsList';
 
-function Dashboard() {
-    const dispatch = useDispatch();
+export default function Dashboard() {
     return (
         <div>
-            {/* <p className="view-title center">Dashboard</p> */}
             <CheckinsList />
             <Navbar />
         </div>
     );
 }
-
-// this allows us to use <App /> in index.js
-export default Dashboard;
