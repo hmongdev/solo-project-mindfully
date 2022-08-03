791e0e6ee00e2fb66be53c48675a7511f39c0d72
Avery Yang <averyyang2674@gmail.com>
2022-08-03 11:02:10 -0500

    add styling to profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Navbar from '../Navbar/Navbar';
+import './Profile.css';
 
 export default function Profile() {
+    const user = useSelector((store) => store.user);
+    const dispatch = useDispatch();
+
     return (
         <>
-            <button
-                className="btn"
-                onClick={() => dispatch({ type: 'LOGOUT' })}
-            >
-                Log Out
-            </button>
+            <p className="view-title">Profile</p>
+            <div className="settings-container">
+                <ul>
+                    <h1 className="greeting">Hello, {user.name}</h1>
+                    <li>
+                        <button>
+                            <span>Edit Name</span>
+                        </button>
+                    </li>
+                    <hr></hr>
+                    <li>
+                        <button onClick={() => dispatch({ type: 'LOGOUT' })}>
+                            <span>Log Out</span>
+                        </button>
+                    </li>
+                    <hr></hr>
+                    <li>
+                        <button>
+                            <span>Delete Account</span>
+                        </button>
+                    </li>
+                </ul>
+            </div>
             <Navbar />
         </>
     );
