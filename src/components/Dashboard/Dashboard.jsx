7f81d699292c943fa67bdb246cbb7c1052a230a7
Avery Yang <averyyang2674@gmail.com>
2022-08-06 16:37:13 -0500

    add css: overflow-x:hidden; fixed table setting

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -32,7 +32,7 @@ export default function Dashboard() {
                     {historyList.map((detail) => {
                         return (
                             <>
-                                <tr>{detail.date_created}</tr>
+                                <tr className="date">{detail.date_created}</tr>
                                 <tr
                                     key={detail.id}
                                     onClick={() =>
