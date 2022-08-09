3ec8838e9d7db9b27dca98ea7f40dcd16755e0b2
Avery Yang <averyyang2674@gmail.com>
2022-08-09 17:25:51 -0500

    testing comment

--- src/components/LoginSpotify/LoginSpotify.js
+++ src/components/LoginSpotify/LoginSpotify.js
@@ -4,6 +4,8 @@ import { Container } from 'react-bootstrap';
 const AUTH_URL =
     'https://accounts.spotify.com/authorize?client_id=218e4b111a3c45bb9c394966af04924c&response_type=code& redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
 
+    //testing comments
+
 export default function Login() {
     return (
         <Container
