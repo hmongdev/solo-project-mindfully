306d0f246b7fc8950606fa9cdd8bd81de1449d19
Avery Yang <averyyang2674@gmail.com>
2022-08-08 23:38:43 -0500

    create login button authentication for spotify

--- src/components/Practices/Practices.jsx
+++ src/components/Practices/Practices.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from '../Login/Login';
 
 export default function Practices() {
     return (
         <div>
             <p className="view-title">Practices</p>
+            <Login />
             <Navbar />
         </div>
     );
