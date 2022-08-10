bb0bfe7a6468ffb332682fd7d75b1e4adcd4fa7d
Avery Yang <averyyang2674@gmail.com>
2022-08-10 13:35:29 -0500

    create spotify player component

--- src/components/LoginSpotify/LoginSpotify.js
+++ src/components/LoginSpotify/LoginSpotify.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 
-const AUTH_URL =
-    'https://accounts.spotify.com/authorize?client_id=218e4b111a3c45bb9c394966af04924c&response_type=code& redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
+export default function LoginSpotify() {
+    const AUTH_URL =
+        'https://accounts.spotify.com/authorize?client_id=218e4b111a3c45bb9c394966af04924c&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
 
-    //testing comments
+    // const CLIENT_ID = '218e4b111a3c45bb9c394966af04924c';
+    // const REDIRECT_URI = 'http://localhost:3000';
+    // const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+    // const RESPONSE_TYPE = 'token';
 
-export default function Login() {
     return (
         <Container
             className="d-flex justify-content-center align-items-center"
             style={{ minHeight: '100vh' }}
         >
-            <a className="btn btn-success btn-lg" href={AUTH_URL}>
-                Login With Spotify
+            <a
+                className="btn btn-success btn-lg"
+                // href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`
+                href={AUTH_URL}
+            >
+                Spotify Login
             </a>
         </Container>
     );
---
+++ src/components/SpotifyPlayer/SpotfifyPlayer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Code from '../Code/Code';
+
+export default function SpotfifyPlayer({ code }) {
+    return <Code code={code} />;
+}
