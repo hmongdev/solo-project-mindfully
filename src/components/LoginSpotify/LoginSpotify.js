b922dd4a6f317e468db60ef1ca5a49decd0e0809
Avery Yang <averyyang2674@gmail.com>
2022-08-11 14:17:21 -0500

    complete styling for LoginSpotifyButton

--- src/components/LoginSpotify/LoginSpotify.js
+++ src/components/LoginSpotify/LoginSpotify.js
@@ -5,13 +5,8 @@ export default function LoginSpotify() {
     const AUTH_URL =
         'https://accounts.spotify.com/authorize?client_id=4b42633281ef4a1ebc3b7359ebb42b4e&response_type=code&redirect_uri=http://localhost:3000/practices&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
     return (
-        <Container
-            className="d-flex justify-content-center align-items-center"
-            style={{ minHeight: '100vh' }}
-        >
-            <a className="btn btn-success btn-lg" href={AUTH_URL}>
-                Spotify Login
-            </a>
+        <Container className="spotify-login-button d-flex justify-content-center align-items-center">
+            <a href={AUTH_URL}>Spotify Login</a>
         </Container>
     );
 }
