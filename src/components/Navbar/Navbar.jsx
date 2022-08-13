9ea45e4c5ce0ad09a0124efaaff4b9b4b3a09959
Avery Yang <averyyang2674@gmail.com>
2022-08-13 15:26:16 -0500

    minor changes to styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,27 +11,20 @@ import HomeIcon from '@mui/icons-material/Home';
 
 export default function Navbar() {
     const history = useHistory();
-    const [clicked, setClicked] = useState(true);
 
     return (
         <div className="navbar">
             <button
+                className="nav-button"
                 onClick={() => {
-                    setClicked(!clicked);
                     history.push('/dashboard');
                 }}
             >
-                {/* <HomeIcon style={{ fontSize: 60, color: 'red' }} /> */}
-                <HomeIcon
-                    style={{
-                        fontSize: 60,
-                        '&:focus': { color: 'purple' },
-                        color: clicked ? 'red' : 'green',
-                    }}
-                />
+                <HomeIcon style={{ fontSize: 60 }} />
                 Dashboard
             </button>
             <button
+                className="nav-button"
                 onClick={() => {
                     history.push('/practices');
                 }}
@@ -40,6 +33,7 @@ export default function Navbar() {
                 Practices
             </button>
             <button
+                className="nav-button"
                 onClick={() => {
                     history.push('/profile');
                 }}
