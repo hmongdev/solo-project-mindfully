3cb28a9050bae61ff4468fab49a8f48b8498f27a
Avery Yang <averyyang2674@gmail.com>
2022-08-19 18:26:47 -0500

    change redirect_uri to herokuapp.com

--- src/components/LoginSpotify/LoginSpotify.js
+++ src/components/LoginSpotify/LoginSpotify.js
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 
 export default function LoginSpotify() {
     const AUTH_URL =
-        'https://accounts.spotify.com/authorize?client_id=4b42633281ef4a1ebc3b7359ebb42b4e&response_type=code&redirect_uri=http://localhost:3000/practices&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
+        'https://accounts.spotify.com/authorize?client_id=4b42633281ef4a1ebc3b7359ebb42b4e&response_type=code&redirect_uri=https://prime-solo-project-mindfully.herokuapp.com/#/practices&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
     return (
         <Container className="spotify-login-button">
             <a href={AUTH_URL}>Spotify Login</a>
